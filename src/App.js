e57f3192ff75af8847ac7c21e33354f65546ca63
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-17 11:53:58 -0500

    created a counter using functional component and hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
 import { Component, useState } from 'react';
 import './App.css';
 
-// function App() {
-//   const [ count, setCount ] = useState(0);
-
-//   return (
-//     <div>
-//       <div>{ count }</div>
-//       <div>Functional Component</div> 
-//     </div>
-//   );
-// }
-
-
-class App extends Component{
-  constructor(){
-    super();
-    this.state={
-      count: 0,
-    }
+function App() {
+  const [ count, setCount ] = useState(0);
+  // const [ word, setWord ] = useState("Hello World")
+  const handleIncrement=()=>{
+    setCount(count + 1)
   }
 
-  handleIncrement=()=>{
-    this.setState({
-      count: this.state.count + 1,
-    })
-  }
-
-  render(){
-    return(
-      <div>
-        <button onClick={this.handleIncrement}>Increment</button>
-        <div>Count: {this.state.count}</div>
-        <div>Class Component</div>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Functional Component</h1> 
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{ count }</div>
+    </div>
+  );
 }
+
+
+// class App extends Component{
+//   constructor(){
+//     super();
+//     this.state={
+//       count: 0,
+//       word: "Hello world"
+//     }
+//   }
+
+//   handleIncrement=()=>{
+//     this.setState({
+//       count: this.state.count + 1,
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <h1>Class Component</h1>
+//         <button onClick={this.handleIncrement}>Increment</button>
+//         <div>Count: {this.state.count}</div>
+//       </div>
+//     )
+//   }
+// }
 
 
 export default App;
