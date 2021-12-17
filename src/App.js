5f329a5e5d63736796b41a8fa03d0ac88bd4ff46
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-17 11:40:20 -0500

    created a counter using a class component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [ count, setCount ] = useState(0);
+
+//   return (
+//     <div>
+//       <div>{ count }</div>
+//       <div>Functional Component</div> 
+//     </div>
+//   );
+// }
+
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      count: 0,
+    }
+  }
+
+  handleIncrement=()=>{
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <div>Count: {this.state.count}</div>
+        <div>Class Component</div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
