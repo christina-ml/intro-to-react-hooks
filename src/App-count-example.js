b134297feecdac869f9a7741b8cc9e1abd4fd75d
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-17 12:31:58 -0500

    moved count example into new file App-count-example.js. Going to practice getting API data using Fetch using hooks

---
+++ src/App-count-example.js
@@ -0,0 +1,67 @@
+import { Component, useState } from 'react';
+import './App.css';
+
+function App() {
+  // const [ word, setWord ] = useState("Hello World")
+  const [ count, setCount ] = useState(0);
+  const [ amount, setAmount ] = useState(1);
+
+  const handleIncrement=()=>{
+    setCount(count + amount)
+  }
+
+  const handleDecrement=()=>{
+    setCount(count - amount)
+  }
+
+  const handleChangeAmount=(e)=>{
+    setAmount(Number(e.target.value))
+  }
+
+  return (
+    <div>
+      <h1>Functional Component</h1>
+      <input type="number" value={amount} onChange={handleChangeAmount} />
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <div>{ count }</div>
+    </div>
+  );
+}
+
+
+// class App extends Component{
+//   constructor(){
+//     super();
+//     this.state={
+//       count: 0,
+//       word: "Hello world"
+//     }
+//   }
+
+//   handleIncrement=()=>{
+//     this.setState({
+//       count: this.state.count + 1,
+//     })
+//   }
+
+//   handleDecrement=()=>{
+//     this.setState({
+//       count: this.state.count - 1,
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <h1>Class Component</h1>
+//         <button onClick={this.handleIncrement}>Increment</button>
+//         <button onClick={this.handleDecrement}>Decrement</button>
+//         <div>Count: {this.state.count}</div>
+//       </div>
+//     )
+//   }
+// }
+
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -2,66 +2,31 @@ import { Component, useState } from 'react';
 import './App.css';
 
 function App() {
-  // const [ word, setWord ] = useState("Hello World")
-  const [ count, setCount ] = useState(0);
-  const [ amount, setAmount ] = useState(1);
-
-  const handleIncrement=()=>{
-    setCount(count + amount)
-  }
-
-  const handleDecrement=()=>{
-    setCount(count - amount)
-  }
-
-  const handleChangeAmount=(e)=>{
-    setAmount(Number(e.target.value))
-  }
 
   return (
     <div>
       <h1>Functional Component</h1>
-      <input value={amount} onChange={handleChangeAmount} />
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
-      <div>{ count }</div>
+
     </div>
   );
 }
 
 
-// class App extends Component{
-//   constructor(){
-//     super();
-//     this.state={
-//       count: 0,
-//       word: "Hello world"
-//     }
-//   }
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
 
-//   handleIncrement=()=>{
-//     this.setState({
-//       count: this.state.count + 1,
-//     })
-//   }
+    }
+  }
 
-//   handleDecrement=()=>{
-//     this.setState({
-//       count: this.state.count - 1,
-//     })
-//   }
-
-//   render(){
-//     return(
-//       <div>
-//         <h1>Class Component</h1>
-//         <button onClick={this.handleIncrement}>Increment</button>
-//         <button onClick={this.handleDecrement}>Decrement</button>
-//         <div>Count: {this.state.count}</div>
-//       </div>
-//     )
-//   }
-// }
-
+  render(){
+    return(
+      <div>
+        <h1>Class Component</h1>
+      </div>
+    )
+  }
+}
 
 export default App;
